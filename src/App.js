b0581db90f7581024ebea7edd884a2cdfafca940
PaulaMontiel <p_dany_mr@hotmail.com>
2022-10-18 00:23:24 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ import MyContext from "./my_context";
 export default function App() {
 
   const [photos, setPhotos] = useState([]);
-  const endpoint = "/fotos.json";
+  const endpoint = '/desafio-naturalpic/fotos.json';
 
   const consultarInformacion = async () => {
     const response = await fetch(endpoint);
     const { photos } = await response.json();
+    console.log(photos);
     photos.forEach((element, id,alt,liked,src) => {
     });
     setPhotos(photos);
@@ -29,7 +30,7 @@ export default function App() {
   return (
     <div className="App">
       <MyContext.Provider value={{photos,setPhotos}}>
-        <BrowserRouter>
+        <BrowserRouter basename="desafio-naturalpic">
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
